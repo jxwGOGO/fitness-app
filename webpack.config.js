3c24fc448442a5463524b82baf1f8b410f678094
JXW007666 <695946606@qq.com>
2021-05-14 16:20:23 +0800

    多页面打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 /**
  * webpack配置文件
  */
+//自动清除dist 
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 //引入 提取js中的css代码的插件
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 //将css文件及代码进行极致压缩s
@@ -18,11 +22,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // 把webpack的配置暴露出去
 module.exports = {
     // 1. 入口
-    entry: './src/js/home.js',
+    entry: {
+        home: './src/js/home.js',
+        login: './src/js/login.js',
+        register: './src/js/register.js'
+    },
     // 2. 出口
     output: {
         path: path.resolve(__dirname, 'dist'), // 输出路径 必须是绝对路径
-        filename: 'js/home.js', // 输出的文件名
+        filename: 'js/[name].js', // 输出的文件名
         publicPath: './', //打包完成之后的html文件引入其他资源的基础路径（相对路径）
     },
     // 3. 配置loader
@@ -73,13 +81,27 @@ module.exports = {
     // 4. 插件
     plugins: [
         new HtmlWebpackPlugin({ //配置html打包的插件
-            template: './src/home.html' //以哪个html文件作为打包的模板
+            template: './src/home.html', //以哪个html文件作为打包的模板
+            filename: 'home.html',
+            chunks: ['home']
+        }),
+        new HtmlWebpackPlugin({ //配置html打包的插件
+            template: './src/login.html', //以哪个html文件作为打包的模板
+            filename: 'login.html',
+            chunks: ['login']
+        }),
+        new HtmlWebpackPlugin({ //配置html打包的插件
+            template: './src/login.html', //以哪个html文件作为打包的模板
+            filename: 'register.html',
+            chunks: ['register']
         }),
         new MiniCssExtractPlugin({
-            filename: 'css/home.css' // 输出到css文件夹里
+            filename: 'css/[name].css' // 动态输出到css文件夹里
         }),
-        new OptimizeCssAssetsWebpackPlugin()
+        new OptimizeCssAssetsWebpackPlugin(),
         // // 引入path模块，path是node内置的模块 主要用于处理路径拼接  ;
+        //plugin 添加
+        new CleanWebpackPlugin()
 
     ],
     //热更新
